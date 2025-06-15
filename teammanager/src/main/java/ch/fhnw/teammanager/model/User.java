c3eb6f9c38b6a5d8374ef3807705a958cf01c443
Anna <133098923+annageiser@users.noreply.github.com>
2025-06-15 21:06:54 +0000

    Füge Annotation für die Tabelle in der User-Klasse hinzu

--- teammanager/src/main/java/ch/fhnw/teammanager/model/User.java
+++ teammanager/src/main/java/ch/fhnw/teammanager/model/User.java
@@ -3,6 +3,7 @@ package ch.fhnw.teammanager.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
