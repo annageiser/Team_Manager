6dfc93ad9c5e2afefb26aa4ae826fe9463f1dbe1
Sharbel Yakoub <sharbel.yakoub@hotmail.com>
2025-06-15 23:22:17 +0200

    Enhance security configuration by improving password handling and refining CORS settings for better API accessibility.

---
+++ teammanager/src/main/java/ch/fhnw/teammanager/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package ch.fhnw.teammanager.repository;
+
+import ch.fhnw.teammanager.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+} 
