5a08bd38e5eb56dcbaa8e5066297991ef45088af
Sharbel Yakoub <sharbel.yakoub@hotmail.com>
2025-06-15 23:19:28 +0200

    Refactor security configuration to improve password handling and enhance CORS support for API accessibility.

---
+++ teammanager/src/main/java/ch/fhnw/teammanager/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ch.fhnw.teammanager.repository;
+
+import ch.fhnw.teammanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+} 
